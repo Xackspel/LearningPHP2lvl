7648b79416ee7f163681de407a02da46fcdf8cfc
Xackspel <42944802+Xackspel@users.noreply.github.com>
2019-02-21 20:26:20 +0200

    Index
    Created index file.

---
+++ index.php
@@ -0,0 +1,14 @@
+<?
+    require_once "config.php";
+    //require_once .php;
+
+    //Header
+    require_once "includes/header.php";
+
+
+    //Body
+    require_once "includes/body.php";
+
+    //Footer
+    require_once "includes/footer.php";
+?>
