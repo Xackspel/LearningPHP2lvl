9fe1ccfc2d98d20e0ea23214edd283d7170dc46b
Xackspel <42944802+Xackspel@users.noreply.github.com>
2019-02-21 21:51:25 +0200

    Attached comments.
    Attached comments which should explain all code.

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <?
-    require_once "config.php";
-    //require_once .php;
+    require_once "config.php"; // Switch on config file
 
+    /*** Website Design ****/
     //Header
-    require_once "includes/header.php";
-
+    require_once "includes/header.php"; // Switch on header;
 
     //Body
-    require_once "includes/body.php";
+    require_once "includes/body.php"; // Switch on body;
 
     //Footer
-    require_once "includes/footer.php";
+    require_once "includes/footer.php"; // Switch on footer;
 ?>
