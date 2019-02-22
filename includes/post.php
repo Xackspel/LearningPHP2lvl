141ddb7a5d9c02542cea79df04849875c22845b3
Xackspel <42944802+Xackspel@users.noreply.github.com>
2019-02-23 00:13:38 +0200

    Updated post template.
    1. Inserted default image;
    2. Created new buttons.

--- includes/post.php
+++ includes/post.php
@@ -1,9 +1,12 @@
 <br>
 <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
+  <img src="<?=$DefaultImage;?>" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Card title</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <button type="button" class="btn btn-primary btn-sm">New</button>
+    <button type="button" class="btn btn-success btn-sm">Read</button>
+    <button type="button" class="btn btn-warning btn-sm">Edit</button>
+    <button type="button" class="btn btn-danger btn-sm">Delete</button>
   </div>
 </div>
