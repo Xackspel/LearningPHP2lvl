0c6b0a06e4e4e8b640a068020835df48fd8ce4ea
Xackspel <42944802+Xackspel@users.noreply.github.com>
2019-02-28 21:06:44 +0200

    Updated function.
    Updated function.

--- functions/functions.php
+++ functions/functions.php
@@ -1,13 +1,9 @@
 <?
-    /** Get all posts and put them as associated array **/
-    function GetAllPosts(){
+    /** Get connection to DB and return as variable **/
+    function ConnectToDataBase(){
         include './config.php';
         $pdo = new PDO($MySQL_Path, $DataBaseLogin, $DataBasePass); // Connection to Data Base;
-        $statement = $pdo -> query("SELECT * FROM posts"); // Selection all posts;
-        $posts = $statement -> fetchall(PDO::FETCH_ASSOC); // Transforming posts to array;
-        return $posts; // Return array of posts;
+        return $pdo;
     }
     
-
-
 ?>
