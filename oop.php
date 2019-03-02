c26ce54668d275d6283f554f3e0467578a880074
Xackspel <42944802+Xackspel@users.noreply.github.com>
2019-03-02 23:36:44 +0200

    Learning OOP.
    Learning OOP.

--- oop.php
+++ oop.php
@@ -6,25 +6,34 @@
         public $speed = 160; #default parameters;
         public $fuel = "gas"; #default parameters;
 
+        # Constuct and Destruct;
+        public function __construct($color, $wheel, $brand, $speed, $fuel){
+            $this->color=$color;
+            $this->wheel=$wheel;
+            $this->brand=$brand;
+            $this->speed=$speed;
+            $this->fuel=$fuel;
+            echo "<br> New object of class ".__CLASS__." created";
+            echo "<br> Method ".__METHOD__." called";
+        }
+
         # This function receive distance and calculate time trip;
         public function range($km){
             
             $range = $km / $this->speed;
             return $range;
         }
+
+        
     }
 
     # Distance
     $distance = 300;
 
-    $car1 = new Car;
-    $car1->color="red";
-    $car1->wheel="17";
-    $car1->brand="Lanos";
-    $car1->speed=220;
-    $car1->fuel="Electric";
+    $car1 = new Car("red", 17, "Lanos", 220, "Electric");
     
 
+    /*
 
     $car2 = new Car;
     $car2->color="blue";
@@ -39,11 +48,14 @@
     $car3->brand="Mercedes";
     $car3->fuel="Gybrid";
     
+    */
 
     echo '<pre>';
     echo "<br>car1 :".$car1->range($distance);
-    echo "<br>car2 :".$car2->range($distance);
-    echo "<br>car3 :".$car3->range($distance);
+    var_dump($car1);
+    //echo "<br>car2 :".$car2->range($distance);
+    //echo "<br>car3 :".$car3->range($distance);
+
     
     
 ?>
