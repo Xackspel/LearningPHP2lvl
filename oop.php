fa3ecb457fb84a9eb69b98da2c0ec1b6c4bfed4f
Xackspel <42944802+Xackspel@users.noreply.github.com>
2019-03-02 16:23:15 +0200

    Learning OOP

--- oop.php
+++ oop.php
@@ -1,15 +1,21 @@
 <?
     class Car {
-        public $color = "white";
-        public $wheel = "16";
-        public $brand = "BMW";
-        public $speed = 160;
-        public $fuel = "gas";
+        public $color = "white"; #default parameters;
+        public $wheel = "16"; #default parameters;
+        public $brand = "BMW"; #default parameters;
+        public $speed = 160; #default parameters;
+        public $fuel = "gas"; #default parameters;
 
-        public function test(){
-            echo 'Test function';
+        # This function receive distance and calculate time trip;
+        public function range($km){
+            
+            $range = $km / $this->speed;
+            return $range;
         }
     }
+
+    # Distance
+    $distance = 300;
 
     $car1 = new Car;
     $car1->color="red";
@@ -17,7 +23,7 @@
     $car1->brand="Lanos";
     $car1->speed=220;
     $car1->fuel="Electric";
-    $car1->test();
+    
 
 
     $car2 = new Car;
@@ -25,12 +31,19 @@
     $car2->wheel="21";
     $car2->brand="Audi";
     $car2->fuel="Electric";
-    $car2->test();
+    
 
     $car3 = new Car;
+    $car3->color="black";
+    $car3->wheel="15";
+    $car3->brand="Mercedes";
+    $car3->fuel="Gybrid";
+    
+
     echo '<pre>';
-    var_dump($car1);
-    var_dump($car2);
+    echo "<br>car1 :".$car1->range($distance);
+    echo "<br>car2 :".$car2->range($distance);
+    echo "<br>car3 :".$car3->range($distance);
     
     
 ?>
