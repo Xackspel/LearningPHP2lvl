2a947ec043c86918bbd96de151cb47bb36b37ad0
Xackspel <42944802+Xackspel@users.noreply.github.com>
2019-03-03 17:04:38 +0200

    Learning OOP

--- oop.php
+++ oop.php
@@ -1,61 +1,22 @@
 <?
-    class Car {
-        public $color = "white"; #default parameters;
-        public $wheel = "16"; #default parameters;
-        public $brand = "BMW"; #default parameters;
-        public $speed = 160; #default parameters;
-        public $fuel = "gas"; #default parameters;
 
-        # Constuct and Destruct;
-        public function __construct($color, $wheel, $brand, $speed, $fuel){
-            $this->color=$color;
-            $this->wheel=$wheel;
-            $this->brand=$brand;
-            $this->speed=$speed;
-            $this->fuel=$fuel;
-            echo "<br> New object of class ".__CLASS__." created";
-            echo "<br> Method ".__METHOD__." called";
+    class Car{
+        public $carColor;
+        public $carBrand;
+        public $carProducet;
+
+        public function __construct($color, $brand, $producet){
+            $this->carColor=$color;
+            $this->carBrand=$brand;
+            $this->carProducet=$producet;
         }
-
-        # This function receive distance and calculate time trip;
-        public function range($km){
-            
-            $range = $km / $this->speed;
-            return $range;
-        }
-
-        
     }
 
-    # Distance
-    $distance = 300;
+    $Lanos = new Car("red","Daewoo", 2004);
 
-    $car1 = new Car("red", 17, "Lanos", 220, "Electric");
-    
+    $test=['alfa','beta', 'gamma'];
+    var_dump($Lanos, $test);
 
-    /*
+    echo $Lanos->carColor;
 
-    $car2 = new Car;
-    $car2->color="blue";
-    $car2->wheel="21";
-    $car2->brand="Audi";
-    $car2->fuel="Electric";
-    
-
-    $car3 = new Car;
-    $car3->color="black";
-    $car3->wheel="15";
-    $car3->brand="Mercedes";
-    $car3->fuel="Gybrid";
-    
-    */
-
-    echo '<pre>';
-    echo "<br>car1 :".$car1->range($distance);
-    var_dump($car1);
-    //echo "<br>car2 :".$car2->range($distance);
-    //echo "<br>car3 :".$car3->range($distance);
-
-    
-    
 ?>
