70d56e41969e5abee42e0ef5ea8304712993f7e6
Xackspel <42944802+Xackspel@users.noreply.github.com>
2019-03-24 20:09:58 +0200

    Learning OOP

--- oop.php
+++ oop.php
@@ -1,158 +1,43 @@
 <?
-
+    # Car class;
     class Car{
-        private $carColor;
         private $carBrand;
-        private $carProducet;
-        private $carEngine;
+        private $carModel;
+        private $carType;
+        private $carColor;
+        private $carYear;
 
-        # Construction of object with checking of type Engine;
-        # If $ngine is't class of Eninge program will stop running;
-        public function __construct($color, $brand, $producet, EngineInterFace $newEngine){
+        public function __construct($brand, $model, $type, $color, $year){
+            $this->updateCarBrand($brand);
+            $this->carType=$type;
             $this->carColor=$color;
-            $this->carBrand=$brand;
-            $this->carProducet=$producet;
-            $this->carEngine=$newEngine;
+            $this->carYear=$year;
         }
 
-        # Start Engine;
-        public function StartEngine(){
-            $this->carEngine->on();
+        public function ShowCarDetails(){
+            echo "Brand:"."&nbsp".$this->carBrand."<br>";
+            echo "Model:"."&nbsp".$this->carModel."<br>";
+            echo "Type:"."&nbsp".$this->carType."<br>";
+            echo "Color:"."&nbsp".$this->carColor."<br>";
+            echo "Year:"."&nbsp".$this->carYear."<br>";
         }
 
-        # Stop Engine;
-        public function StopEngine(){
-            $this->carEngine->off();
-        }
-
-        # Calculate range in km;
-        public function range($distance_km){
-
-        }
-
-        # Change car color;
-        public function changeColor($newColor){
-            $this->carColor=$newColor;
-        }
-
-        # Show car details;
-        public function carDetails($tempVarr){
-            if($tempVarr == "Color"){
-                return $this->carColor;
+        public function UpdateCarBrand($brand){
+            if(is_string($brand)){
+                $this->carBrand=$brand;
+                echo "Car brand have been updated<br>";
             }
-            if($tempVarr == "Brand"){
-                return $this->carBrand;
+            else{
+                die("Please enter car color in string format!"); 
             }
-            if($tempVarr == "Producet"){
-                return $this->carProducet;
-            }
-            #unset($tempVarr);
-            #var_dump($tempVarr);
         }
     }
 
-    interface EngineInterFace{
-        public function on();
-        public function off();
-    }
-
-    class Engine implements EngineInterFace{
-        public function on(){
-            echo "Start Gas Engine";
-        }
-
-        public function off(){
-            echo "Stop Gas Engine";
-        }
-    }
-
-    class anotherEngine implements EngineInterFace{
-        public function on(){
-            echo "Start Diesel Engine";
-        }
-
-        public function off(){
-            echo "Stop Diesel Engine";
-        }
-    }
-    
-    $engine = new Engine;
-    $anotherEngine = new anotherEngine;
-
-    $Passat = new Car("black","VolksWagen", 2002, $engine);
-    $Passat->StartEngine();
-    $Passat->StopEngine();
-    #var_dump($Passat);
-
-    $Lanos = new Car("red","Daewoo", 2004, $anotherEngine);
-    $Lanos->StartEngine();
-    $Lanos->StopEngine();
-    #var_dump($Lanos);
-    $Lanos->changeColor("blue");
-    echo "<br>".$Lanos->carDetails("Color");
-    echo "<br>".$Lanos->carDetails("Brand");
-    echo "<br>".$Lanos->carDetails("Producet")."<br>";
+    # New object of car class;
+    $myCar = new Car("Daewoo", "Lanos", "Sedan", "Red", 2004);
+    $myCar->ShowCarDetails();
+    $myCar->UpdateCarBrand("Chevrolet");
+    $myCar->ShowCarDetails();
 
 
-
-    abstract class Animal{
-        public $animalName;
-        public $animalColor;
-        public $animalWeight;
-        public $animalSound;
-
-        public  function __construct($name, $color, $weight, $sound){
-            $this->animalName=$name;
-            $this->animalColor=$color;
-            $this->animalWeight=$weight;
-            $this->animalSound=$sound;
-        }
-
-        #public abstract function makeSound();
-    }
-
-    class Cat extends Animal{
-        # some code;
-        public  function makeSound(){
-            echo $this->animalSound."<br>";
-        }
-    }
-    
-    $myCat = new Cat("murchik", "black", 8, "meow");
-    $myCat->makeSound();
-
-    #var_dump($myCat);
-    
-    trait Greetings{
-        
-        public function sayHi(){
-            $this;
-            echo "Hi";
-        }
-        public function greetPerson(){
-            $personName = $this->personName;
-            echo "Wellcome".'&nbsp'.$personName."<br>";
-        }
-        
-        public function makeSound(){
-            $animalSound = $this->$animalSound;
-            echo $animalSound."<br>";
-        }
-    }
-
-    class Person{
-        public $personName;
-        public $personAge;
-        use Greetings;
-
-        public function __construct($name, $age){
-            $this->personName=$name;
-            $this->personAge=$age;
-        }
-    }
-
-    $Oleksii = new Person("Oleksii", 34);
-    #var_dump($Oleksii);
-    $Oleksii->greetPerson();
-    $Oleksii->sayHi();
 ?>
