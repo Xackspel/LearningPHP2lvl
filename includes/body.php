a57298338e12fbd65f48f56aba6acdb15c8d9c95
Xackspel <42944802+Xackspel@users.noreply.github.com>
2019-03-25 23:34:43 +0200

    Updated design
    Updated design

--- includes/body.php
+++ includes/body.php
@@ -1,9 +1,12 @@
 <body>
     <? require_once "navbar.php"; // Switch on navbar;?>
     <br>
-    <div class="container-fluid">
-        <div class="row">
-            <? require "post.php"; // Switch on navbar;?>
+
+    <div class="container">
+        <div class="container-fluid">
+            <div class="row">
+                <? require "post.php"; // Switch on navbar;?>
+            </div>
         </div>
     </div>
 </body>
--- includes/navbar.php
+++ includes/navbar.php
@@ -1,13 +1,15 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="index.php"><?=$Logo?></a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
-            <a class="nav-item nav-link active" href="index.php">Home</a>
-            <a class="nav-item nav-link" href="#">About</a>
-            <a class="nav-item nav-link" href="#">About</a>
+<div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="index.php"><?=$Logo?></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="index.php">Home</a>
+                <a class="nav-item nav-link" href="#">About</a>
+                <a class="nav-item nav-link" href="#">About</a>
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
+</div>
