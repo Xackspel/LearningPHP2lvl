da260e4e75db853dcf1c80c97b1c17393a76d4cd
Xackspel <42944802+Xackspel@users.noreply.github.com>
2019-03-25 23:35:39 +0200

    Inserted link for button
    Inserted link for button

--- includes/post.php
+++ includes/post.php
@@ -16,13 +16,13 @@
   //var_dump($posts);
 ?>
 <?foreach($posts as $post):?>
- <div class="col-sm">
+ <div class="col">
     <div class="card" style="width: 18rem;">
       <img src="<?=$DefaultImage;?>" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title"><?=$PostName = $post['post_name'];?></h5>
         <p class="card-text"><?=$PostDescription = $post['post_description'];?></p>
-        <button type="button" class="btn btn-primary btn-sm">New</button>
+        <button type="button" class="btn btn-primary btn-sm" onclick="document.location='create.php'">New</button>
         <button type="button" class="btn btn-success btn-sm">Read</button>
         <button type="button" class="btn btn-warning btn-sm">Edit</button>
         <button type="button" class="btn btn-danger btn-sm">Delete</button>
