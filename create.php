7646634f66f0dfa401df52f10f8a6984f3544a47
Xackspel <42944802+Xackspel@users.noreply.github.com>
2019-03-25 23:33:32 +0200

    Template for creation new post.
    Template for creation new post.

---
+++ create.php
@@ -0,0 +1,13 @@
+<?
+    require_once "config.php"; // Switch on config file
+
+    /*** Website Design ****/
+    //Header
+    require_once "includes/header.php"; // Switch on header;
+
+    //Body
+    require_once "includes/newpost.php"; // Switch on body;
+
+    //Footer
+    require_once "includes/footer.php"; // Switch on footer;
+?>
