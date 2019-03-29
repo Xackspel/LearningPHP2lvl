084a23b1f840ea181482912bea9f2e63fea79740
Xackspel <42944802+Xackspel@users.noreply.github.com>
2019-03-29 22:32:23 +0200

    Required HTML input
    Required HTML input

--- includes/newpost.php
+++ includes/newpost.php
@@ -5,16 +5,19 @@
         <div class="col">
         <form action="attaching_post.php" method="POST">
             <div class="form-group">
+                <img src="<?=$DefaultImage;?>" alt="Please select image." name="post_image">
+            </div>
+            <div class="form-group">
                 <label for="exampleInputEmail1">Post Name</label>
-                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter post name" name="pos_tname">
+                <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter post name" name="post_tname" required>
             </div>
             <div class="form-group">
                 <label for="exampleInputPassword1">Post Description</label>
-                <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Enter post description" name="post_description">
+                <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Enter post description" name="post_description" required>
             </div>
             <div class="form-group">
                 <label for="exampleInputPassword1">Post Text</label>
-                <textarea name="Post_Text" class="form-control" id="exampleFormControlTextarea1" rows="10"></textarea>
+                <textarea type="text" class="form-control" id="exampleFormControlTextarea1" placeholder="Enter post text here" rows="10" name="post_text" required></textarea>
             </div>
             </div>
                 <button type="submit" class="btn btn-primary">Create new post</button>
