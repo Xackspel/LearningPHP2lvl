6b8069b5a35af758adcb496105f8d008af139fe2
Xackspel <42944802+Xackspel@users.noreply.github.com>
2019-03-29 22:32:34 +0200

    Removed old code.
    Removed old code.

--- includes/post.php
+++ includes/post.php
@@ -2,18 +2,11 @@
   include './config.php';
   include './functions/functions.php';
   include './classs/querybuilder.php';
-  $pdo = ConnectToDataBase(); // Get Connection to Data Base;
-  $db = new QueryBuilder($pdo);
-  $posts = $db->GetAll('posts');
-  
-  //$posts = GetAllPosts($pdo); // Showing all posts.
+  include './classs/database.php';
 
-  // 'post_name'
-  // 'post_description'
-  // 'post_text'
-  // 'author_id'
-  // 'post_image'
-  //var_dump($posts);
+  $pdo = ConnectToDataBase(); // Get Connection to Data Base;
+  $dataBase = new QueryBuilder($pdo);
+  $posts = $dataBase->GetAll('posts');
 ?>
 <?foreach($posts as $post):?>
  <div class="col">
