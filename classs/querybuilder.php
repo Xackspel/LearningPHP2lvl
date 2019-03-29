3ee69a6915753dd2029975e15b3baa63ee8d5f9d
Xackspel <42944802+Xackspel@users.noreply.github.com>
2019-03-29 22:31:59 +0200

    Updated QB
    Updated QB

--- classs/querybuilder.php
+++ classs/querybuilder.php
@@ -1,12 +1,12 @@
 <?
     class QueryBuilder{
         protected $pdo;
-        
+
         public function __construct($pdo){
             $this->pdo=$pdo;
         }
 
-        /** Get all posts and put them as associated array **/
+        /** Get all from $table = table name in data base and return them as associated array **/
         public function GetAll($table){
             $statement = $this->pdo->query("SELECT * FROM {$table}"); // Selection all posts;
             $result = $statement -> fetchall(PDO::FETCH_ASSOC); // Transforming posts to array;
